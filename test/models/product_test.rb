f1d0681f9fe498df94ac131d400b8b91faff6921
hashjane <hashjane@gmail.com>
2018-02-08 11:50:30 +1300

    validations and unit test using fixtures in controllers and models

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
@@ -57,4 +58,16 @@ class ProductTest < ActiveSupport::TestCase
 
   end
 
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:    products(:ruby).title,
+                          description:  "yyy",
+                          price:  1,
+                          image_url:  "fred.gif")
+
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+#
 end
