b64d55f6c51facc1a4a25c9cbeaae0aa875aa775
hashjane <hashjane@gmail.com>
2018-02-09 10:31:51 +1300

    Playtime: wrapping number_to_currency to have the preferred currency

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,7 @@
 class StoreController < ApplicationController
+  include ActionView::Helpers::NumberHelper
+  helper_method :number_to_nzd
+
   def index
     @products = Product.order(:title)
     @time = time_display 
@@ -7,6 +10,10 @@ class StoreController < ApplicationController
   def time_display
     time = Time.now
     time.to_formatted_s(:short)
+  end
+
+  def number_to_nzd(price)
+    number_to_currency(price, unit: "NZD$", separator: ".", delimiter: "", format: "%u %n")
   end
 
 end
