522ac02a384708e950559850fea67620d9f5389f
hashjane <hashjane@gmail.com>
2018-02-08 17:41:11 +1300

    Playtime: add date and time to the sidebar

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @time = time_display 
   end
+
+  def time_display
+    time = Time.now
+    time.to_formatted_s(:short)
+  end
+
 end
