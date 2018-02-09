980e2d5f9a542e9ebb3a05d6b8c40475a905aa46
hashjane <hashjane@gmail.com>
2018-02-09 14:43:00 +1300

    Add Cart and fixed StoreController test for the new currency

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,32 @@
 class Product < ApplicationRecord
 
-validates :title, :description, :image_url, :price, presence: true
+  has_many :line_items
 
-validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  before_destroy :ensure_not_referenced_by_any_line_item
 
-validates :title, uniqueness: true
+  validates :title, :description, :image_url, :price, presence: true
 
-validates :image_url, allow_blank: true, format: {
-    with:     %r{\.(gif|jpg|png)\Z}i,
-    message:  'must be a URL for GIF, JPG or PNG image.'
-  }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
-validates :title, length: { minimum: 10,
-  too_short: "should have at least %{count} characters"}
+  validates :title, uniqueness: true
+
+  validates :image_url, allow_blank: true, format: {
+      with:     %r{\.(gif|jpg|png)\Z}i,
+      message:  'must be a URL for GIF, JPG or PNG image.'
+    }
+
+  validates :title, length: { minimum: 10,
+    too_short: "should have at least %{count} characters"}
+
+
+  private
+  
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 
 end
