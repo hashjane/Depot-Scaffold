3902bfcad37e2b1a72e68c573ebf6a145f690548
hashjane <hashjane@gmail.com>
2018-02-12 10:48:50 +1300

    applied css and Playtime: display how many times the customer access the store and display the counter if the count is greater than 5

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,9 +2,21 @@ class StoreController < ApplicationController
   include ActionView::Helpers::NumberHelper
   helper_method :number_to_nzd
 
+  def increment_session_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+
+    session[:counter] += 1;
+  end
+
+
   def index
+    @session_counter = increment_session_counter
     @products = Product.order(:title)
-    @time = time_display 
+    @time = time_display
+    @session_counter_message = "You have been here for #{@session_counter} times" if @session_counter > 5
+
   end
 
   def time_display
