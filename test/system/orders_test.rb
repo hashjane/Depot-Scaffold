ee33d708cb926b957ef82c93284109eb55644e5a
hashjane <hashjane@gmail.com>
2018-02-14 13:47:05 +1300

    learn to write system test

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -6,4 +6,22 @@ class OrdersTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "Order"
   # end
+
+  test "check routing number" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Hazel Jane'
+    fill_in 'order_address', with: '123 Street'
+    fill_in 'order_email', with: 'hash@hash.com'
+    
+    assert_no_selector "#order_routing_number"
+
+    select 'Check', from: 'pay_type'
+
+    assert_selector "#order_routing_number"
+  end
 end
